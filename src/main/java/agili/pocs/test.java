b704bf4eca10acb6b58c8cb641c046ec8cb93ae4
karimzrouga <zrougakarim@gmail.com>
2023-09-08 13:01:33 -0700

    first commit

---
+++ src/main/java/agili/pocs/test.java
@@ -0,0 +1,13 @@
+package agili.pocs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+	@GetMapping("/test")
+	public String test() {
+		return "ffff";
+	}
+
+}
